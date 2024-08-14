c757528f73b0ca41c931b51c7c94e682eaf67e38
Wang <e1101919@asp2a-login-nus01.head.cm.asp2a.nscc.sg>
2024-08-14 10:12:53 +0800

    muscle

--- Analysis/3.muscle_bulk/03.differential-expression.R
+++ Analysis/3.muscle_bulk/03.differential-expression.R
@@ -1,29 +1,26 @@
-# Muscle bulk - 03. differential-expression
-# Load necessary packages
+# 3.muscle bulk - 03. differential expression
 library(edgeR)
 library(statmod)
 
 # Load saved RData files
 load("SAMS2_muscle_count_table.RData")  # loads expression_res
 load("SAMS2_muscle_TPM.RData")          # loads tpm
+load("sample_info.RData")               
 
-# Define groups based on column names
 group <- factor(ifelse(grepl("Pre", colnames(expression_res)), "Pre", "Post"))
-group <- relevel(group, "Pre")  # Set the control group to 1
+group <- relevel(group, "Pre")  # Set the control group to 'Pre'
 
-# Construct design matrix
-design <- model.matrix(~0 + group)
-rownames(design) <- colnames(expression_res)
-colnames(design) <- levels(group)
+sample_info <- sample_info[match(colnames(expression_res), sample_info$ID), ]
 
-# Create DGEList object and filter lowly expressed genes
+design <- model.matrix(~ group + Age + factor(ID))
+
 dge <- DGEList(counts = expression_res, group = group)
 dge <- calcNormFactors(dge, method = 'TMM')  # Normalize library sizes
 dge <- estimateDisp(dge, design, robust = TRUE)
 
 # Fit the model and perform quasi-likelihood F-test
 fit <- glmQLFit(dge, design, robust = TRUE)
-lt <- glmQLFTest(fit, contrast = c(-1, 1))
+lt <- glmQLFTest(fit, contrast = c(-1, 1, rep(0, ncol(design)-2)))  # Adjust contrast to test the effect of 'group'
 
 # Get differentially expressed genes
 tempDEG <- topTags(lt, n = Inf)
@@ -31,8 +28,8 @@ tempDEG <- as.data.frame(tempDEG)
 nrDEG <- na.omit(tempDEG)
 
 # Classify gene expression changes
-nrDEG$gene_type <- ifelse(nrDEG$PValue > 0.05, 'Not', 
-                          ifelse(nrDEG$logFC > 1, 'Up', 
+nrDEG$gene_type <- ifelse(nrDEG$PValue > 0.05, 'Not',
+                          ifelse(nrDEG$logFC > 1, 'Up',
                                  ifelse(nrDEG$logFC < -1, 'Down', 'Not')))
 table(nrDEG$gene_type)
 
@@ -41,3 +38,4 @@ up_down <- nrDEG[grep("Up|Down", nrDEG$gene_type), ]
 up_down1 <- cbind(rownames(up_down), up_down)
 colnames(up_down1)[1] <- "ID"
 write.table(up_down1, "DEG.csv", sep = ",", row.names = FALSE, col.names = TRUE)
+
--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # muscle-QTL
 
-This repository contains scripts to analyze the Singapore Adult Metabolism Study (SAMS) phase 2 data for the manuscript "Genetic Effects in Response to Exercise and Diet-induced Weight-loss in Overweight Individuals.
+This repository contains scripts to analyze the Singapore Adult Metabolism Study phase 2 (SAMS2) data for the manuscript "Genetic Effects in Response to Exercise and Diet-induced Weight-loss in Overweight Individuals.
 
-Code and scripts for analysis and figures are categorized according to sections from the main manuscript. 
+Scripts for analysis and figures are categorized according to sections from the main manuscript. 
